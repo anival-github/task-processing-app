c2af38370a9fb139ef9e91bbe9535720438612e0
Admin <mr.valentin.anikin@gmail.com>
2025-04-21 19:50:37 +0300

    first

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,48 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
-import { ReactiveFormsModule } from '@angular/forms';
-import { FormsModule } from '@angular/forms';
-
-import { NgxsModule } from '@ngxs/store';
-import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
-import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CoreModule } from './core/core.module';
-import { SharedModule } from './shared/shared.module';
-import { TaskState } from './store/task.state';
-import { TaskSubmissionModule } from './features/task-submission/task-submission.module';
-import { TaskDashboardModule } from './features/task-dashboard/task-dashboard.module';
-import { FeaturesModule } from './features/features.module';
-
-import { environment } from '../environments/environment';
+import { HeaderComponent } from './components/header/header.component';
+import { TaskFormComponent } from './components/task-form/task-form.component';
+import { TaskDashboardComponent } from './components/task-dashboard/task-dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TaskFormComponent,
+    TaskDashboardComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
-    AppRoutingModule,
-    CoreModule,
-    SharedModule,
-    HttpClientModule,
-    ReactiveFormsModule,
-    TaskSubmissionModule,
-    TaskDashboardModule,
-    FormsModule,
-    FeaturesModule,
-    NgxsModule.forRoot([TaskState], {
-      developmentMode: !environment.production
-    }),
-    NgxsLoggerPluginModule.forRoot(),
-    NgxsReduxDevtoolsPluginModule.forRoot()
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { } 
--- frontend/src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://api.yourdomain.com' // We'll update this with the actual production API URL later
-}; 
