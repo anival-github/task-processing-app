edb93079c04e5c62b1b170d4e9ed5ee21bb5a973
Admin <mr.valentin.anikin@gmail.com>
2025-05-01 12:27:57 +0300

    clean-up-structure

--- frontend/src/app/components/task-form/task-form.component.ts
+++ frontend/src/app/components/task-form/task-form.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
 import { Store } from '@ngxs/store';
 import { AddTask } from '../../store/tasks/tasks.actions';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-task-form',
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule
+  ],
   templateUrl: './task-form.component.html',
   styleUrls: ['./task-form.component.scss']
 })
